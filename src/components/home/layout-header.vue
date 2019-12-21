5a6153fab213c53e04cb6f658d29074157e4cc4c
theshy <1449881277@qq.com>
2019-12-21 21:33:12 +0800

    头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -7,12 +7,12 @@
       <el-col class="right" :span="12">
           <el-row type='flex' justify="end" align="middle">
               <img :src="!userInfo.photo ? userInfo.photo : defaultImg" alt="">
-              <el-dropdown>
+              <el-dropdown @command="clickMenu">
                   <span>{{userInfo.name}}</span>
                   <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>个人信息</el-dropdown-item>
-                      <el-dropdown-item>git地址</el-dropdown-item>
-                      <el-dropdown-item>退出</el-dropdown-item>
+                      <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                      <el-dropdown-item command="git">git地址</el-dropdown-item>
+                      <el-dropdown-item command="goout">退出</el-dropdown-item>
                   </el-dropdown-menu>
               </el-dropdown>
           </el-row>
@@ -38,6 +38,18 @@ export default {
     }).then(result => {
       this.userInfo = result.data.data
     })
+  },
+  methods: {
+    clickMenu (command) {
+      if (command === 'info') {
+
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/S7seven/heimatoutiao'
+      } else {
+        window.localStorage.removeItem('user-token')
+        this.$router.push('/login')
+      }
+    }
   }
 }
 </script>
