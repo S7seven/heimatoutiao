72246ac3d421810466a6e3e5c42600265b908d4e
theshy <1449881277@qq.com>
2019-12-21 21:47:06 +0800

    左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,23 +3,23 @@
       <div class="title">
           <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-      <el-menu style="background-color:rgb(86, 207, 255)">
-          <el-menu-item>首页</el-menu-item>
+      <el-menu router style="background-color:rgb(86, 207, 255)">
+          <el-menu-item index="/home">首页</el-menu-item>
           <el-submenu index="1">
               <template slot="title">内容管理</template>
-              <el-menu-item>发布文章</el-menu-item>
-              <el-menu-item>文章列表</el-menu-item>
-              <el-menu-item>评论管理</el-menu-item>
-              <el-menu-item>素材管理</el-menu-item>
+              <el-menu-item index="/home/publish">发布文章</el-menu-item>
+              <el-menu-item index="/home/articles">文章列表</el-menu-item>
+              <el-menu-item index="/home/comment">评论管理</el-menu-item>
+              <el-menu-item index="/home/material">素材管理</el-menu-item>
           </el-submenu>
            <el-submenu index="2">
               <template slot="title">粉丝管理</template>
-              <el-menu-item>图文数据</el-menu-item>
-              <el-menu-item>粉丝概况</el-menu-item>
-              <el-menu-item>粉丝画像</el-menu-item>
-              <el-menu-item>粉丝列表</el-menu-item>
+              <el-menu-item index="/home/picture">图文数据</el-menu-item>
+              <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+              <el-menu-item index="/home/fanspicture">粉丝画像</el-menu-item>
+              <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
           </el-submenu>
-          <el-menu-item>账户信息</el-menu-item>
+          <el-menu-item index="/home/userinfo">账户信息</el-menu-item>
       </el-menu>
   </div>
 </template>
